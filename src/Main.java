20b2a6d3b3f491265f1ac3577f5b9e6884baa5ca
alonso1337 <pasha.eliseev1008@gmail.com>
2026-02-12 16:02:43 +0300

    tusk 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        int r = 5;
-        double x = (4.0 / 3.0) * 3.14 * r * r * r;
-        System.out.println(x);
+public class Main{
+    public static void main(String[] args){
+        double x = 4;
+        double y = 2;
+        double summ = (1 + y) * (2 * x + y * y - (x + y) / (y + 1 / (x * x - 4)));
+        System.out.println(summ);
+
     }
 }
