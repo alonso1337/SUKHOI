92d6cb030f9916cddcc179f88295511bfcff3b26
alonso1337 <pasha.eliseev1008@gmail.com>
2026-02-12 15:31:06 +0300

    tusk 2

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,7 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите x: ");
-        double x = scanner.nextDouble();
-
-        System.out.print("Введите y: ");
-        double y = scanner.nextDouble();
-
-        if (y == 0) {
-            System.out.println("Ошибка: деление на ноль!");
-            scanner.close();
-            return;
-        }
-
-        System.out.print("Введите z: ");
-        double z = scanner.nextDouble();
-
-       ;
-        System.out.println("Результат: " + ((x + 2) * y - z) / y + y * z);
-        System.out.println("!");
-
-        scanner.close();
+        int r = 5;
+        double x = (4.0 / 3.0) * 3.14 * r * r * r;
+        System.out.println(x);
     }
 }
