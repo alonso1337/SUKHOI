30a3d30b884a1347e54e61a9c88947b0bcbf4213
alonso1337 <pasha.eliseev1008@gmail.com>
2026-02-12 12:59:36 +0300

    tusk 1

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите x: ");
+        double x = scanner.nextDouble();
+
+        System.out.print("Введите y: ");
+        double y = scanner.nextDouble();
+
+        if (y == 0) {
+            System.out.println("Ошибка: деление на ноль!");
+            scanner.close();
+            return;
+        }
+
+        System.out.print("Введите z: ");
+        double z = scanner.nextDouble();
+
+       ;
+        System.out.println("Результат: " + ((x + 2) * y - z) / y + y * z);
+
+        scanner.close();
+    }
+}
